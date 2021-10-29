f6baeb1ec71399612f02ad65b5cf72570f51b55c
Abexo-koss <93327057+Abexo-koss@users.noreply.github.com>
2021-10-29 11:24:42 +0300

    Update Juntastringgenenator.py

--- Juntastringgenenator.py
+++ Juntastringgenenator.py
@@ -5,8 +5,7 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 
-ccheck = input("Enter junta to continue: ")
-if ccheck == "junta":
+
     print("Please go to my.telegram.org and get your API Id and API Hash to proceed.")
     APP_ID = int(input("Enter APP ID here: "))
     API_HASH = input("Enter API HASH here: ")
@@ -15,7 +14,4 @@ if ccheck == "junta":
         print(client.session.save())
         print('check your save message!')
         client.send_message("me", client.session.save())
-        client.send_message("me", "Above is your #JUNTA STRING SESSION ,,, kelay yalew yerso #JUNTA USERBOT STRING SESSION  new yehnen  session lemanm endaysetu \nPaste this string in Heroku Var.\n\n[Developer Abex kos](t.me/JUNTAUSERBOT")
-
-else:
-    print("yetesasate neger asgebtewal")
+        client.send_message("me", "Above is your #JUNTA STRING SESSION ,,, kelay yalew yerso #JUNTA USERBOT STRING SESSION  new yehnen  session lemanm endaysetu \nPaste this string in Heroku Var.\n\n[Developer Abex Kos ] \n\n [t.me/JUNTAUSERBOT].")
